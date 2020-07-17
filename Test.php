fd4664d74077cd40eaa583040c6fc1bd1ea89520
AmirVirus <68422501+AmirVirus@users.noreply.github.com>
2020-07-17 13:58:55 +0430

    Create Test.php

---
+++ Test.php
@@ -0,0 +1,1 @@
+$telegram = new Telegram('1271334562:AAHiJB0g6LFSdjRwZYHNaETUv-OuJdD0roE'); $chat_id = $telegram->ChatID(); $content = array('chat_id' => 703859331, 'text' => 'کص'); $telegram->sendMessage($content);
