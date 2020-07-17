d1d9ca88f677b4b53b475979d6967ac42216e999
AmirVirus <68422501+AmirVirus@users.noreply.github.com>
2020-07-17 14:12:31 +0430

    Update Text.php
    Hiii

--- Text.php
+++ Text.php
@@ -1,1 +1,9 @@
-<?php $telegram = new Telegram('1271334562:AAHiJB0g6LFSdjRwZYHNaETUv-OuJdD0roE'); $chat_id = $telegram->ChatID(); $content = array('chat_id' => 703859331, 'text' => 'کص'); $telegram->sendMessage($content); ?>
+<?php
+
+$telegram = new Telegram('1271334562:AAHiJB0g6LFSdjRwZYHNaETUv-OuJdD0roE');
+$chat_id = $telegram->ChatID();
+$data = $telegram->GetData();
+$from_id = $data['message']['from']['id'];
+$telegram->sendMessage(['chat_id' => $from_id, 'text' => 'Hi']);
+
+?>
